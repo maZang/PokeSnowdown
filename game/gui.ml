aec57d6ebcc7998c6d69a504d1f37a80b0a91ce6
Matthew Zang <msz45.com>
2015-11-13 22:33:19 -0500

    gui and info

--- game/gui.ml
+++ game/gui.ml
@@ -1,88 +1,80 @@
-open Async.Std 
+open Async.Std
 
 let screen_width = 642
 let screen_height= 480
 
-let locale = GtkMain.Main.init () 
+let locale = GtkMain.Main.init ()
 
-let current_screen = ref Info.MainMenu 
+let current_screen = ref Info.MainMenu
 
 (* Make all the menu items for the game loading screen *)
-let make_menu ~file1 ~file2?packing () = 
-	let hbox = GPack.hbox ~homogeneous:true ?packing () in 
-	let vbox1 = GPack.vbox ~packing:(hbox#pack) () in 
-	let vbox2 = GPack.vbox ~packing:(hbox#pack) () in 
-	let vbox3 = GPack.vbox ~packing:(hbox#pack) () in 
+let make_menu ~file1 ~file2?packing () =
+	let hbox = GPack.hbox ~homogeneous:true ?packing () in
+	let vbox1 = GPack.vbox ~packing:(hbox#pack) () in
+	let vbox2 = GPack.vbox ~packing:(hbox#pack) () in
+	let vbox3 = GPack.vbox ~packing:(hbox#pack) () in
 	let _ = GMisc.image ~file:file1 ~packing:(vbox1#pack) () in
-	let _ = GMisc.image ~file:file2 ~height:460 
+	let _ = GMisc.image ~file:file2 ~height:460
 		~packing:(vbox3#pack ~expand:true) () in
 	let button1 = GButton.button ~label:"1-Player"
-		~packing:(vbox2#pack ~expand:true ~fill:true) () in 
+		~packing:(vbox2#pack ~expand:true ~fill:true) () in
 	let button2 = GButton.button ~label:"2-Player"
-		~packing:(vbox2#pack ~expand:true ~fill:true) () in 
+		~packing:(vbox2#pack ~expand:true ~fill:true) () in
 	let button3 = GButton.button ~label:"No Player"
-		~packing:(vbox2#pack ~expand:true ~fill:true) ()  in 
-	let vbox4 = GPack.vbox () in 
+		~packing:(vbox2#pack ~expand:true ~fill:true) ()  in
 	let button4 = GButton.button ~label:"Random Battle"
-		~packing:(vbox4#pack ~expand:true ~fill:true) ()  in 
+		~packing:(vbox2#pack ~expand:true ~fill:true) ~show:false () in
 	let button5 = GButton.button ~label:"Preset Battle"
-		~packing:(vbox4#pack ~expand:true ~fill:true) ()  in 
-	let button6 = GButton.button ~label:"Tournament" 
-		~packing:(vbox4#pack ~expand:true ~fill:true) ()  in 
+		~packing:(vbox2#pack ~expand:true ~fill:true) ~show:false () in
+	let button6 = GButton.button ~label:"Tournament"
+		~packing:(vbox2#pack ~expand:true ~fill:true) ~show:false () in
 	let button7 = GButton.button ~label:"Back"
-		() in 
-	hbox, vbox2, button1, button2, button3, vbox4, button4,
-		button5, button6, vbox3, button7
+		~packing:(vbox3#pack ~from:`END) () ~show:false in
+  hbox, vbox1, vbox2, vbox3, button1, button2, button3, button4,
+		button5, button6, button7
 
-let load_not_main_menu engine menu_holder menu1 menu2 buffer_area 
-	back_button() = 
-	if Ivar.is_empty (!engine) then 
-		(menu_holder#remove buffer_area#coerce;
-		menu_holder#remove menu1#coerce;
-		menu_holder#add menu2#coerce;
-		buffer_area#add back_button#coerce;
-		menu_holder#add buffer_area#coerce;
-		current_screen := Info.Menu1P;
-		Ivar.fill !engine Info.Menu1P)
+let load_not_main_menu engine button1 button2 button3 button4 button5 button6
+  back_button() =
+	if Ivar.is_empty (!engine) then
+		(List.iter (fun s -> s#misc#hide ())[button1; button2; button3];
+    List.iter (fun s -> s#misc#show ()) [button4;button5;button6;
+    back_button]; current_screen := Info.Menu1P; Ivar.fill !engine Info.Menu1P)
 	else
-		() 
+		()
 
-let load_main_menu engine menu_holder main_menu menu1 buffer_area
-	back_button () = 
-	if Ivar.is_empty (!engine) then 
-		(menu_holder#remove buffer_area#coerce;
-		menu_holder#remove menu1#coerce;
-		menu_holder#add main_menu#coerce;
-		buffer_area#remove back_button#coerce;
-		menu_holder#add buffer_area#coerce;
-		current_screen := Info.MainMenu;
-		Ivar.fill !engine Info.MainMenu)
+let load_main_menu engine one_player two_player no_player button4 button5
+  button6 back_button () =
+	if Ivar.is_empty (!engine) then
+		(List.iter (fun s -> s#misc#hide ()) [button4;button5;button6;
+      back_button];
+    List.iter (fun s -> s#misc#show ())[one_player; two_player; no_player];
+		current_screen := Info.MainMenu; Ivar.fill !engine Info.MainMenu)
 	else
-		() 
+		()
 
-let go_back engine (menu_holder, main_menu, one_player, two_player, no_player,
-		one_player_menu, random_1p, preset_1p, touranment, buffer_area,
-		back_button) () = 
-	if !current_screen = Info.Menu1P then 
-		load_main_menu engine menu_holder main_menu one_player_menu
-			buffer_area back_button ()
+let go_back engine (menu_holder, left_side, main_menu, right_side, one_player,
+    two_player, no_player, random_1p, preset_1p, touranment,
+    back_button) () =
+	if !current_screen = Info.Menu1P then
+		load_main_menu engine one_player two_player no_player random_1p preset_1p
+      touranment back_button ()
 
 
 (* The main gui *)
-let main_gui engine () = 
+let main_gui engine () =
 	let window = GWindow.window ~width: screen_width ~height: screen_height
-		~title: "Pokemon Snowdown" ~resizable:false () in 
+		~title: "Pokemon Snowdown" ~resizable:false () in
 	(* menu = menu_holder, main_menu, one_player, two_player, no_player,
 		one_player_menu, random_1p, preset_1p, touranment, buffer_area,
 		back_button *)
-	let menu = make_menu 
-		~file1:"./gui_pics/bg1.png" ~file2:"./gui_pics/bg2.png" 
-		~packing:(window#add) () in 
-	let menu_holder, main_menu, one_player, two_player, no_player,
-		one_player_menu, random_1p, preset_1p, touranment, buffer_area,
-		back_button = menu in 
-	one_player#connect#clicked ~callback:(load_not_main_menu engine menu_holder 
-		main_menu one_player_menu buffer_area back_button);
+	let menu = make_menu
+		~file1:"./gui_pics/bg1.png" ~file2:"./gui_pics/bg2.png"
+		~packing:(window#add) () in
+	let menu_holder, left_side, main_menu, right_side, one_player,
+		two_player, no_player, random_1p, preset_1p, touranment,
+		back_button = menu in
+	one_player#connect#clicked ~callback:(load_not_main_menu engine one_player
+  two_player no_player random_1p preset_1p touranment back_button);
 	back_button#connect#clicked ~callback:(go_back engine menu);
 	window#show ();
-	let thread = GtkThread.start () in () 
+	let thread = GtkThread.start () in ()
--- game/info.mli
+++ game/info.mli
@@ -1,42 +1,54 @@
 (* Info contains all the data types needed for the handling of the game *)
 
-type game_state = MainMenu | Menu1P | Quit 
+type game_state = MainMenu | Menu1P | Quit
 
-type battlemove = SwitchPoke of int | UseAttack of int 
+type battlemove = SwitchPoke of int | UseAttack of int
 
-(* *)
-type move = string 
+type target = SpecificPoke | SelectedPokeMeFirst | Ally | UsersField |
+    UserOrAlly | OpponentsFields | User | RandomOpp | AllOthers |
+    SelectedPoke | AllOpp | EntireField | UserAndAlly | All
 
-type pokemon 
+type dmg_class = Physical | Special | Status
+
+type element = Fire | Water | Grass | Rock | Ground | Fairy | Dark | Electric |
+  Ghost | Steel | Normal | Bug | Flying | Psychic | Ice | Dragon | Fighting |
+  Poison
+
+type move = {name:string; priority: int; target: target; dmg_class: dmg_class;
+    power:int; effect_chance: int; accuracy: int; element: element;
+    description: string}
+
+type pokemon = {element: element list; moves: move list; name: string;
+  hp: int; attack: int; defense:int; special_defense:int; special_attack:int;
+  speed:int; ability:string}
 
 (* variants containing all secondary effects of a given move *)
-type secondary_effects 
-
-(* most likely also a record with also a list of secondary_effects*)
-type attack 
+type secondary_effects
 
 (* We hope to implement all of these below but we will see *)
-type weather_terrain = HarshSun | Hail | Rain | SandStorm 
-	| HeavyRain | Sun | AirCurrent | ClearSkies 
+type weather_terrain = HarshSun | Hail | Rain | SandStorm
+	| HeavyRain | Sun | AirCurrent | ClearSkies
 
-type non_volatile_status = Burn | Freeze | Paralysis | Poison | Toxic | Sleep 
+type non_volatile_status = Burn | Freeze | Paralysis | Poison | Toxic | Sleep
 
-type volatile_status =  Confusion | Curse | Embargo | Flinch | HealBlock 
-	| Identification | Infatuation | Nightmare | Trapped | PerishSong | Leeched 
-	| Taunt | Levitate | Torment 
+type volatile_status =  Confusion | Curse | Embargo | Flinch | HealBlock
+	| Identification | Infatuation | Nightmare | Trapped | PerishSong | Leeched
+	| Taunt | Levitate | Torment
 
-type status = non_volatile_status * volatile_status list 
+type status = non_volatile_status * volatile_status list
 
-type pokemon_type 
+(* stat modifier represents number of stages followed by multiplier *)
+type stat_modifier = int * float
 
-type item 
+type pokemon_stat_modifier = {attack: stat_modifier; defense: stat_modifier;
+    speed: stat_modifier; special_attack: stat_modifier;
+    special_defense: stat_modifier; evasion: stat_modifier;
+    accuracy: stat_modifier}
 
-type pokemon_stat_modifier 
+type trainer_team = {current: pokemon; alive: pokemon list; dead: pokemon list}
 
-type trainer_team
+type battle_state = trainer_team * trainer_team * weather_terrain
 
-type battle_state = trainer_team * trainer_team * weather_terrain 
-
-(* As in competitive Pokemon, there will be no ties. If all Poke die in 
+(* As in competitive Pokemon, there will be no ties. If all Poke die in
 one turn, the one with the Poke standing the latest wins *)
 type outcome = WinnerP1 | WinnerP2
