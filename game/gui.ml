1932a64cfd747a148365b63f96d2fbfe4ad7a91e
Matthew Zang <msz45.com>
2015-11-15 14:08:25 -0500

    gui updates

--- game/gui.ml
+++ game/gui.ml
@@ -22,10 +22,15 @@ let make_battle_screen ?packing () =
      () in
   let poke1_img = GMisc.image ~file:"../data/back-sprites/gyarados.gif" () in
   let poke2_img = GMisc.image ~file:"../data/sprites/aipom.gif" () in
+  let move1 = GButton.button ~label:"Move1" ~show:false () in
+  let move2 = GButton.button ~label:"Move2" ~show:false () in
+  let move3 = GButton.button ~label:"Move3" ~show:false () in
+  let move4 = GButton.button ~label:"Move4" ~show:false () in
+  let switch = GButton.button ~label:"Switch" ~show: false() in
   battle#attach ~left:0 ~top:1 ~bottom:4 poke1_img#coerce;
   battle#attach ~left:3 ~top:0 ~bottom:3 poke2_img#coerce;
   battle#attach ~left:0 ~top:0 ~right:4 ~bottom:4 ~fill:`BOTH bg_img#coerce;
-  battle, text, bg_img
+  battle, text, bg_img, move1, move2, move3, move4, switch
 
 (* Make all the menu items for the game loading screen *)
 let make_menu ?packing () =
@@ -54,52 +59,42 @@ let make_menu ?packing () =
   (vbox, hbox1, hbox2, button1, button2, button3, button4,
 		button5, button6, button7, img, img2)
 
-let load_battle_screen engine img battle text () =
+let load_battle_screen engine img battle text buttonhide buttonshow () =
   if Ivar.is_empty (!engine) then
     (img#misc#hide (); battle#misc#show (); text#misc#show ();
+     List.iter (fun s -> s#misc#hide ()) buttonhide;
+     List.iter (fun s -> s#misc#show ()) buttonshow;
     current_screen := Info.Battle; Ivar.fill !engine Info.Battle)
   else
     ()
 
-let load_not_main_menu engine button1 button2 button3 button4 button5 button6
-  back_button img1 img2 () =
+let load_menu engine button_show button_hide img1 img2 () =
 	if Ivar.is_empty (!engine) then
-		(List.iter (fun s -> s#misc#hide ())[button1; button2; button3];
-    List.iter (fun s -> s#misc#show ()) [button4;button5;button6;
-    back_button]; img1#misc#hide (); img2#misc#show ();
+		(List.iter (fun s -> s#misc#hide ()) button_hide;
+    List.iter (fun s -> s#misc#show ()) button_show;
+    img1#misc#hide (); img2#misc#show ();
     current_screen := Info.Menu1P; Ivar.fill !engine Info.Menu1P)
 	else
 		()
 
-let load_main_menu engine one_player two_player no_player button4 button5
-  button6 back_button main_menu_bg img2 () =
-	if Ivar.is_empty (!engine) then
-		(List.iter (fun s -> s#misc#hide ()) [button4;button5;button6;
-      back_button];
-    List.iter (fun s -> s#misc#show ())[one_player; two_player; no_player];
-    img2#misc#hide (); main_menu_bg#misc#show ();
-		current_screen := Info.MainMenu; Ivar.fill !engine Info.MainMenu)
-	else
-		()
-
-let load_main_menu_from_battle engine one_player two_player no_player button4 button5
-  button6 back_button main_menu_bg battle text () =
-  engine := Ivar.create ();
-  (List.iter (fun s -> s#misc#hide ()) [button4;button5;button6;
-    back_button];
+let load_main_menu_from_battle engine one_player two_player no_player button_hide
+ main_menu_bg battle text () =
+  engine := Ivar.create (); Thread.delay 0.5;
+  (List.iter (fun s -> s#misc#hide ()) button_hide;
   List.iter (fun s -> s#misc#show ())[one_player; two_player; no_player];
   battle#misc#hide (); text#misc#hide (); main_menu_bg#misc#show ();
   current_screen := Info.MainMenu; Ivar.fill !engine Info.MainMenu)
 
 let go_back engine (menu_holder, main_menu, battle_scren, one_player,
     two_player, no_player, random_1p, preset_1p, touranment,
-    back_button, main_menu_bg, one_bg) battle text bg_img () =
+    back_button, main_menu_bg, one_bg) (battle, text, bg_img, move1, move2,
+    move3, move4, switch)  () =
 	(if !current_screen = Info.Menu1P then
-		load_main_menu engine one_player two_player no_player random_1p preset_1p
-      touranment back_button main_menu_bg one_bg ());
+		load_menu engine [one_player;two_player;no_player]
+    [random_1p; preset_1p ;touranment; back_button] one_bg main_menu_bg());
   (if !current_screen = Info.Battle then
-    load_main_menu_from_battle engine one_player two_player no_player random_1p preset_1p
-      touranment back_button main_menu_bg battle text ())
+    load_main_menu_from_battle engine one_player two_player no_player [move1;
+    move2; move3; move4; switch; back_button] main_menu_bg battle text ())
 
 
 (* The main gui *)
@@ -113,11 +108,17 @@ let main_gui engine () =
 	let menu_holder, main_menu, battle_screen, one_player,
 		two_player, no_player, random_1p, preset_1p, touranment,
 		back_button, main_menu_bg, one_bg = menu in
-  let battle, text, bg_img = make_battle_screen ~packing:(battle_screen#add) () in
-	one_player#connect#clicked ~callback:(load_not_main_menu engine one_player
-  two_player no_player random_1p preset_1p touranment back_button main_menu_bg
-  one_bg); back_button#connect#clicked ~callback:(go_back engine menu battle text
-  bg_img ); random_1p#connect#clicked ~callback:(load_battle_screen engine one_bg
-  battle text);
+  let battler = make_battle_screen ~packing:(battle_screen#add) ()
+  in let battle, text, bg_img, move1, move2, move3, move4, switch = battler in
+  main_menu#pack move1#coerce; main_menu#pack move2#coerce;
+  main_menu#pack move3#coerce; main_menu#pack move4#coerce;
+  main_menu#pack switch#coerce;
+	one_player#connect#clicked ~callback:(load_menu engine [random_1p;preset_1p;
+  touranment;back_button] [one_player;
+  two_player;no_player] main_menu_bg one_bg); back_button#connect#clicked
+  ~callback:(go_back engine menu battler);
+  random_1p#connect#clicked ~callback:(load_battle_screen engine one_bg
+  battle text [random_1p;preset_1p;touranment] [move1; move2; move3; move4;
+  switch]);
 	window#show ();
 	let thread = GtkThread.start () in ()
