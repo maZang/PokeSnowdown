73273d24acb09eb49ceae1788f9b8b417de018cc
Matthew Zang <msz45.com>
2015-11-18 18:24:48 -0500

    finished getting random pokemon method

--- game/pokemon.ml
+++ game/pokemon.ml
@@ -55,9 +55,35 @@ let getElement str =
   | "poison" -> Poison
   | _ -> failwith "Not a valid type"
 
+(* Gets a random element in a list *)
 let getRandomElement lst =
   let l = List.length lst in
   lst |> nth (Random.int l)
+
+let getTarget str =
+  match str with
+  | "specific-move" -> SpecificPoke
+  | "selected-pokemon-me-first" -> SelectedPokeMeFirst
+  | "ally" -> Ally
+  | "users-field" -> UsersField
+  | "user-or-ally" -> UserOrAlly
+  | "opponents-field" -> OpponentsFields
+  | "user" -> User
+  | "random-opponent" -> RandomOpp
+  | "all-other-pokemon" -> AllOthers
+  | "selected-pokemon" -> SelectedPoke
+  | "all-opponents" -> AllOpp
+  | "entire-field" -> EntireField
+  | "user-and-allies" -> UserAndAlly
+  | "all-pokemon" -> All
+  | _ -> failwith "Not a valid target"
+
+let getDmgClass str =
+  match str with
+  | "physical" -> Physical
+  | "special" -> Special
+  | "status" -> Status
+  | _ -> failwith "Not a valid damage class"
 
 (* Returns something of form  {name:string; priority: int; target: target; dmg_class: dmg_class;
     power:int; effect_chance: int; accuracy: int; element: element;
@@ -67,7 +93,16 @@ let getMoveFromString str =
   let priority = move |> member "priority" |> to_string |> int_of_string in
   let powerstr = move |> member "power" |> to_string in
   let power = try int_of_string powerstr with |_ -> 0 in
-  ()
+  let dmg_class = move |> member "dmg_class" |> to_string |> getDmgClass in
+  let target = move |> member "target" |> to_string |> getTarget in
+  let effect_chance_str = move |> member "effect_chance" |> to_string in
+  let effect_chance = try int_of_string effect_chance_str with |_ -> 100 in
+  let accuracy_str = move |> member "accuracy" |> to_string in
+  let accuracy = try int_of_string accuracy_str with  |_ -> 100 in
+  let element = move |> member "type" |> to_string |> getElement in
+  let description = move |> member "effect" |> to_string in
+  {name = str; priority; target; dmg_class; power; effect_chance; accuracy;
+  element; description}
 
 let getRandomPokemon () =
   let randomPokeName = poke_arr |>
@@ -76,10 +111,10 @@ let getRandomPokemon () =
   let element_string = randomPoke |> member "type" |> to_list|> filter_string in
   let element = List.map getElement element_string in
   let moves = randomPoke |> member "moves" |> to_list in
-  let move1str = getRandomElement moves |> to_string in
-  let move2str = getRandomElement moves |> to_string in
-  let move3str = getRandomElement moves |> to_string in
-  let move4str = getRandomElement moves |> to_string in
+  let move1s = ref (getRandomElement moves |> to_string) in
+  let move2s = ref (getRandomElement moves |> to_string) in
+  let move3s = ref (getRandomElement moves |> to_string) in
+  let move4s = ref (getRandomElement moves |> to_string) in
   let hp = randomPoke |> member "stats" |> member "hp" |> to_string
             |> int_of_string in
   let attack = randomPoke |> member "stats" |> member "attack" |> to_string
@@ -98,9 +133,17 @@ let getRandomPokemon () =
             hp=84; speed=84} in
   let nature = getRandomNature () in
   let item = getRandomItem () in
-  {name = randomPokeName; element; move1 = move1str; move2 = move2str;
-  move3 = move3str; move4 = move4str; hp = hp; attack; defense;
-  special_defense; special_attack; speed; ability; evs;
+  while (move2s = move1s) do move2s := getRandomElement moves |> to_string done;
+  while (move3s = move2s || move3s = move1s) do move3s :=
+      getRandomElement moves |> to_string done;
+  while (move4s = move3s || move4s = move2s || move4s = move1s) do
+      move4s := getRandomElement moves |> to_string done;
+  let move1 = !move1s |> getMoveFromString in
+  let move2 = !move2s |> getMoveFromString in
+  let move3 = !move3s |> getMoveFromString in
+  let move4 = !move4s |> getMoveFromString in
+  {name = randomPokeName; element; move1 ; move2; move3 ; move4 ; hp;
+  attack; defense; special_defense; special_attack; speed; ability; evs;
   nature; item}
 
 
