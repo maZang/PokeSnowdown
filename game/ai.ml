474494cd8ed35251ee2244f50330e53f0ea34738
Young <yk524@cornell.edu>
2015-11-27 01:25:52 -0500

    improved ai a bit

--- game/ai.ml
+++ game/ai.ml
@@ -15,3 +15,33 @@ let getRandomMove (poke:battle_poke) : string =
 let replaceDead (lst:battle_poke list) : string =
   let n = Random.int (List.length lst) in
     (List.nth lst n).pokeinfo.name
+
+(* [has_advantage] checks whether the element of the pokemon [elst1] has
+ * a type advantage against the element of the opposing pokemon [elst2].
+ *)
+let has_advantage (elst1:element list) (elst2:element list) : bool =
+  let rec helper = function
+    | [] -> 1.
+    | h::t ->
+        (List.fold_left (fun acc x -> acc *. getElementEffect h x) 1. elst2)
+        *. (helper t)
+  in
+  if (helper elst1 >= 2.) then true
+  else false
+
+(* [replaceDead2] returns the string name of the first pokemon in the list of
+ * alive pokemon [alivelst] that has a type advantage against the opposing
+ * pokemon [poke1]. If there are no such pokemon, one is randomly chosen
+ * from the list [alivelst].
+ *)
+let replaceDead2 (poke1:battle_poke) (alivelst:battle_poke list) : string =
+  let rec helper = function
+    | [] -> let n = Random.int (List.length alivelst) in
+            (List.nth alivelst n).pokeinfo.name
+    | h::t ->
+        if (has_advantage h.pokeinfo.element poke1.pokeinfo.element) then
+          h.pokeinfo.name
+        else helper t
+  in
+  if (alivelst = []) then failwith "No Pokemon Left. IDK what to do here"
+  else helper alivelst
