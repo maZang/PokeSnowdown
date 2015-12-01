63f7916f3622e01272534f2b603176bbf14f4b42
Chirag Bharadwaj <cb625@cornell.edu>
2015-12-01 00:44:32 -0500

    Man, it's so loud in here

--- game/pokemon.mli
+++ game/pokemon.mli
@@ -1,22 +1,45 @@
 open Info
 
-(*This module is responsible for getting valid pokemon *)
-val unlocked_poke_string_list: unit -> string list
+(* This module is responsible for getting valid Pokemon. *)
 
-val getRandomPokemon: unit -> pokemon
+(* [unlocked_poke_string_list ()] generates a string list of Pokemon by reading
+ *  data from a JSON file.
+ *)
+val unlocked_poke_string_list : unit -> string list
 
-val getPokeToolTip: trainer_team -> string
+(* [getRandomPokemon ()] returns a random Pokemon, filled with its own abilities
+ *  and initialized with the appropriate characteristics (moves, etc.).
+ *)
+val getRandomPokemon : unit -> pokemon
 
-val getMoveToolTip: move -> string
+(* [getPokeToolTip t] returns a string containing relevant tool-tip information
+ *  (stats, etc.) for team t's current Pokemon.
+ *
+ *  - [t] is the trainer team whose current Pokemon's information is returned.
+ *)
+val getPokeToolTip : trainer_team -> string
 
-val string_of_weather: weather -> string
+(* [getMoveToolTip move] returns a string containing relevant tool-tip
+ *  information (what the move does, etc.) for the specified Pokemon move.
+ *
+ *  - [move] is the name of the Pokemon move whose help information is returned.
+ *)
+val getMoveToolTip : move -> string
 
-val getElementEffect: element -> element -> float
+(*  *)
+val string_of_weather : weather -> string
 
-val getTestPoke: unit -> pokemon
+(*  *)
+val getElementEffect : element -> element -> float
 
-val getTestOpp: unit -> pokemon
+(*  *)
+val getTestPoke : unit -> pokemon
 
-val getMoveFromString: string -> move
+(*  *)
+val getTestOpp : unit -> pokemon
 
-val string_of_element: element -> string
+(*  *)
+val getMoveFromString : string -> move
+
+(*  *)
+val string_of_element : element -> string
