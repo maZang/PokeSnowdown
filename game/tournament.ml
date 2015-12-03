5a65cad87ed5d17ba2ff12b0f61c17c64d20e87b
Matthew Zang <msz45.com>
2015-12-03 15:02:47 -0500

    some changes

--- game/tournament.ml
+++ game/tournament.ml
@@ -5,6 +5,15 @@ type enemy = RoughNeck | Beauty | BugCatcher | CampNerd | DragonTamer | Falkner 
 let enemy1 = ref Beauty
 let enemy2 = ref Falkner
 
+let profOakQuotes = ["Welcome to Pokemon Snowdown.";
+                    "You may pick one of the two trainers to face.";
+                    "Look at the enemy trainers closely to discern what type of Pokemon they use.";
+                    "You will have the option of choosing your Pokemon after you make your selection."]
+
+let opp1Quotes = ["I am Chirag's mom.";
+                    "You will regret choosing me."]
+
+
 let getRandomEnemy () =
   match Random.int 9 with
   | 0 -> RoughNeck
@@ -18,9 +27,8 @@ let getRandomEnemy () =
   | 8 -> Youngster
   | _ -> failwith "Does Not Happen"
 
-let getRandomOpp1 () =
-  let rand_enemy = getRandomEnemy () in
-  let sprite = match rand_enemy with
+let getStringFromEnemy enm =
+  match enm with
   | RoughNeck -> "baldman"
   | Beauty -> "beauty"
   | BugCatcher -> "bugcatcher"
@@ -29,9 +37,17 @@ let getRandomOpp1 () =
   | Falkner -> "falker"
   | FatMan -> "fatman"
   | Psychic -> "psychic"
-  | Youngster -> "youngster" in
+  | Youngster -> "youngster"
+
+let getRandomOpp1 () =
+  let rand_enemy = getRandomEnemy () in
+  let sprite = getStringFromEnemy rand_enemy in
   enemy1 := rand_enemy; sprite
 
+let getRandomOpp2 () =
+  let rand_enemy = getRandomEnemy () in
+  let sprite = getStringFromEnemy rand_enemy in
+  enemy2:= rand_enemy; sprite
 
 let profOakQuotes = ["Welcome to Pokemon Snowdown.";
                     "You may pick one of the two trainers to face.";
@@ -39,7 +55,7 @@ let profOakQuotes = ["Welcome to Pokemon Snowdown.";
                     "You will have the option of choosing your Pokemon after you make your selection."]
 
 
-let opp1Quotes = ["I am Chirag's mom.";
+let opp1Quotes () = ["I am Chirag's mom.";
                     "You will regret choosing me."]
 
 
