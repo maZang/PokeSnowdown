05bb8f15f43f38225673cdc8eec0b94120d501fd
Matthew Zang <msz45.com>
2015-12-04 21:17:14 -0500

    asd

--- game/info.mli
+++ game/info.mli
@@ -12,7 +12,7 @@ type guiattack = NormMove of string | Crit of guiattack |
                   | ConfuseMoveA of guiattack | Recharging of guiattack | DrainA of guiattack | UserFaintA of guiattack | NoEffAll of string | DrainSleepFail of string
                   | BreakSub of guiattack | SubDmg of guiattack | ProtectedA of string | StatBoostA of stat * int * guiattack | SwitchOutA of guiattack | FalseSwipeA of guiattack
                   | ConfuseUserA of guiattack | KnockedOff of item * guiattack | SleepMove of guiattack | SleepAttack of guiattack | SleepAttackFail of string
-                  | TrappingMove of guiattack | NoRecoil of guiattack | LifeOrbA of guiattack
+                  | TrappingMove of guiattack | NoRecoil of guiattack | LifeOrbA of guiattack | RapidSpinA of guiattack
 
 type guistatus = StatBoost of stat * int * guistatus | NormStatus of string | ThawS of guistatus | FrozenSolidS | MissStatus of string | NoFreezeS of guistatus | NoBurnS of guistatus |
                   NoParaS of guistatus | ParaS | SwitchOut of guistatus | FlinchS | StatAttack of stat * int * guistatus | AsleepS | WakeS of guistatus | MakeSleep of guistatus
@@ -22,7 +22,8 @@ type guistatus = StatBoost of stat * int * guistatus | NormStatus of string | Th
                    Fail of string | SpikesS of guistatus | BurnStatus of guistatus | HealBellS of guistatus | RefreshS of guistatus | PsychUpS of guistatus | SunnyDayS of guistatus |
                    RainDanceS of guistatus | SandStormS of guistatus | HailS of guistatus | EncoreS of guistatus | EncoreFail | CopyPrevMoveS of guistatus | CopyPrevMoveA of guiattack
                    | CopyFail | TauntS of guistatus | TauntFail | Taunted of string | StealthRockS of guistatus | ToxicSpikesS of guistatus | StickyWebS of guistatus
-                   | SleepTalkS of guistatus * guistatus | SleepTalkA of guistatus * guiattack | SleepAttackS of guistatus | UserFaintS of guistatus | RandMoveS of guistatus | RandMoveA of guiattack
+                   | SleepTalkS of guistatus * guistatus | SleepTalkA of guistatus * guiattack | SleepAttackS of guistatus | UserFaintS of guistatus | RandMoveS of guistatus | RandMoveA of guiattack |
+                   ItemSwapS of guistatus
 
 type endMove = BurnDmg | BreakBurn | BreakFreeze  | BreakPara  | BreakPoison | PoisonDmg | LeechDmg of endMove | LeechHeal of endMove | Base | LightScreenFade of endMove |
                ReflectFade of endMove | SunFade of endMove | RainFade of endMove | SandStormFade of endMove | SandBuffetB of endMove | SandBuffet1 of endMove |
@@ -60,7 +61,7 @@ type secondary_effects = MultHit of int | StageBoost of (stat * int) list | IncC
                          | Spikes | HealBell | SunHeal | MaxHealthDmg | SunnyDay | FalseSwipe | Refresh | PsychUp | RandStageBoost | FlowerShield | RainDance | SandStormMake
                          | HailMake | Encore of int | PainSplit | SelfEncore | ConfuseUser | CopyPrevMove | KnockOff | ChanceStageBoost | SelfSwitch | FoulPlay | TauntMove
                          | StealthRockMake | TSpikes | StickyWebMake | Rototiller | SleepEffect | BeatUp | CausePartialTrapping | DoublePower | StoredPower | VenomDrench
-                         | RandMove
+                         | RandMove | RapidSpin | ItemSwitch
 
 type move = {name:string; priority: int; target: target; dmg_class: dmg_class;
     mutable power:int; effect_chance: int; accuracy: int; element: element;
--- game/pokemon.ml
+++ game/pokemon.ml
@@ -418,7 +418,7 @@ let getSecondaryEffect str = match str with
   | "thunder-punch" | "body-slam" | "stun-spore" | "thunder-shock"
     | "thunderbolt" | "thunder-wave" | "thunder" | "lick" | "glare"
     | "zap-cannon" | "spark" | "dragon-breath" | "nuzzle" | "secret-power"
-    | "discharge" -> [ParaChance]
+    | "discharge" | "force-palm" -> [ParaChance]
   | "guillotine" | "horn-drill" | "fissure" | "sheer-cold" -> [OHKO]
   | "razor-wind" -> [ChargeMove "It made a whirlwind!"]
   | "fly" -> [ChargeMove "It flew up into the air."]
@@ -434,7 +434,7 @@ let getSecondaryEffect str = match str with
   | "double-kick" | "gear-grind" | "bonemerang" | "double-hit" | "dual-chop" -> [MultHit 2]
   | "sand-attack" | "smokescreen" | "kinesis" | "flash"
     | "mud-slap" | "octazooka" | "leaf-tornado" | "mud-bomb"
-    | "muddy-water" -> [StageAttack [(Accuracy, 1)]]
+    | "muddy-water" | "mirror-shot"-> [StageAttack [(Accuracy, 1)]]
   | "take-down" | "double-edge" | "submission" | "brave-bird" | "wild-charge"
       | "wood-hammer" | "flare-blitz" | "head-smash" | "head-charge" -> [RecoilMove]
   | "tail-whip" | "leer" | "iron-tail" | "crunch"
@@ -568,6 +568,9 @@ let getSecondaryEffect str = match str with
   | "venom-drench" -> [VenomDrench]
   | "v-create" -> [StageBoost [(Defense,-1); (SpecialDefense,-1); (Speed,-1)]]
   | "metronome" -> [RandMove]
+  | "rapid-spin" -> [RapidSpin]
+  | "switcheroo" | "trick" -> [ItemSwitch]
+  | "close-combat" -> [StageBoost [(Defense, -1); (SpecialDefense, -1)]]
   | _ -> []
 
 (* Returns something of form  {name:string; priority: int; target: target; dmg_class: dmg_class;
@@ -582,6 +585,7 @@ let getMoveFromString str =
             | "return" | "frustration" -> 102
             | "heavy-slam" | "heat-crash" -> 120
             | "trump-card" -> 80
+            | "natural-gift" -> 100
             | _ ->  (try int_of_string powerstr with |_ -> 0) in
   let dmg_class = move |> member "dmg_class" |> to_string |> getDmgClass in
   let target = move |> member "target" |> to_string |> getTarget in
@@ -721,9 +725,9 @@ let getTestPoke () =
   let evs = {attack = 0; defense =  255; special_attack= 255; special_defense= 255;
             hp=255; speed=255} in
   let nature = Bold in
-  let item = Leftovers in
-  {name="gardevoir-mega"; element=[Grass]; move1= getMoveFromString "metronome"; move2 =
-  getMoveFromString "v-create"; move3 = getMoveFromString "pound";
+  let item = ChoiceScarf in
+  {name="gardevoir-mega"; element=[Grass]; move1= getMoveFromString "switcheroo"; move2 =
+  getMoveFromString "v-create"; move3 = getMoveFromString "close-combat";
   move4 = getMoveFromString "poison-powder"; hp = 98; attack = 100; special_attack = 165; defense = 65;
   speed = 120; special_defense = 135; ability="drizzle"; evs; nature; item}
 
@@ -731,9 +735,9 @@ let getTestOpp () =
   let evs = {attack = 255; defense =  0; special_attack= 0; special_defense= 255;
             hp=255; speed=255} in
   let nature = Bold in
-  let item = ChoiceScarf in
+  let item = Leftovers in
   {name="gallade-mega"; element=[Grass;Flying]; move1= getMoveFromString "roost"; move2 =
-  getMoveFromString "sand-tomb"; move3 = getMoveFromString "earthquake";
+  getMoveFromString "sand-tomb"; move3 = getMoveFromString "rapid-spin";
   move4 = getMoveFromString "toxic"; hp = 68; attack = 255; special_attack = 165; defense = 65;
   speed = 100; special_defense = 135; ability="gale-wings"; evs; nature; item}
 
